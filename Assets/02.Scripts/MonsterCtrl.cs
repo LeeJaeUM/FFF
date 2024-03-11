2ce851d62ca009c5af0d61a06a9277eeb9d61f54
kkc <94228190+arumolru@users.noreply.github.com>
2024-03-11 14:56:30 +0900

    임현택 제작
    몬스터 추적 제작

---
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterCtrl : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator anim;
+    Vector3 vec;
+
+    [SerializeField]
+    private Transform skyLight; // directional light
+    [SerializeField]
+    private Transform[] area; // 몬스터의 이동 루트 변수
+    [SerializeField]
+    private Transform player; // 플레이어 위치
+    private float chaseDuration = 10f; // 추적을 유지할 시간
+
+    private float totalTime = 30; // 낮과 밤이 바뀌는 시간
+    private float nowTime = 0;
+    private float chaseTimer = 0; // 추적을 유지하는 타이머
+    private float pursuitRange = 3f; // 플레이어를 인식하는 범위
+
+    private bool isMonsterMove = false; // 몬스터의 이동 상황
+    private int current = 0; // 몬스터의 이동 순서 변수
+    private bool isChasing = false; // 추적 중인지 여부확인
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+
+        // 초기 목적지 설정
+        SetDestinationByIndex(current);
+    }
+
+    private void Update()
+    {
+        nowTime += Time.deltaTime;
+
+        if (nowTime >= totalTime)
+        {
+            RotateSkyLight();
+            isMonsterMove = !isMonsterMove;
+            nowTime = 0;
+
+            SetNextDestination();
+        }
+
+        if (isMonsterMove == true && area != null && area.Length>0)
+        {
+            // 플레이어와의 거리 계산
+            float distanceToPlayer=Vector3.Distance(transform.position, player.position);
+
+            // 추적 시작 조건
+            if(player != null && distanceToPlayer <= pursuitRange)
+            {
+                isChasing = true;
+                chaseTimer = 0;
+
+                agent.SetDestination(player.position);
+                anim.SetBool("IsWalk", true);
+            }
+
+            // 추적 중이면서 지정된 시간 이내에 있는 경우 계속 추적
+            if(isChasing && chaseTimer<chaseDuration)
+            {
+                agent.SetDestination(player.position);
+                chaseTimer += Time.deltaTime;
+            }
+
+            else
+            {
+                float remainingDistance = agent.remainingDistance;
+
+                if (remainingDistance <= agent.stoppingDistance)
+                {
+                    // 현재 목적지에 도착했으면 다음 목적지 설정
+                    SetNextDestination();
+                    isChasing = false;
+                }
+
+                anim.SetBool("IsWalk", true);
+            }
+        }
+        else
+        {
+            agent.ResetPath();
+            anim.SetBool("IsWalk", false);
+        }
+    }
+
+    void RotateSkyLight()
+    {
+        if (skyLight != null)
+        {
+            skyLight.transform.Rotate(180f, 0, 0);
+        }
+    }
+
+    void SetNextDestination()
+    {
+        if(area!=null && area.Length>0)
+        {
+            current = (current + 1) % area.Length;
+
+            // 다음 목적지로 이동
+            SetDestinationByIndex(current);
+        }
+    }
+
+    void SetDestinationByIndex(int index)
+    {
+        if(area != null && area.Length>index)
+        {
+            agent.SetDestination(area[index].position);
+        }
+    }
+}
---
+++ Assets/02.Scripts/thc6anim.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thc6anim : MonoBehaviour
+{
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+}
