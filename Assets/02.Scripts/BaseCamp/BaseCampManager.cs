e89561c6c5792bfd4f10a99c6cb03a5eec438246
LeeJaeUng <lsam3333333@gmail.com>
2024-03-12 14:06:16 +0900

    BaseCampManager 싱글톤 제거 후 일반 클래스로 변경

--- Assets/02.Scripts/BaseCamp/BaseCampManager.cs
+++ Assets/02.Scripts/BaseCamp/BaseCampManager.cs
@@ -2,8 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseCampManager : Singleton<BaseCampManager>
+public class BaseCampManager : MonoBehaviour
 {
+    public static BaseCampManager instance;
+    public static BaseCampManager Instance
+    {
+        get
+        {
+            if(instance == null)
+                instance = FindAnyObjectByType<BaseCampManager>();
+            return instance;
+        }
+    }
+
+
     [SerializeField]Workbench workbench;
     public Workbench Workbench 
     { 
