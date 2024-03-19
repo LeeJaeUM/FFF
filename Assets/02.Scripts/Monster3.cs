77a0971ceb073385449d42f492098a34f6a4ac57
kkc <94228190+arumolru@users.noreply.github.com>
2024-03-19 14:56:37 +0900

    몬스터 3,4,5 기믹 구현
    3 : 플레이어의 시야에 몬스터 태그가 인지 되면 몬스터가 쫒아옴.
    4 : 뒤 돌아보면 몬스터한테 죽음
    5 : 화면이 흔들리면 몬스터한테 죽음

--- Assets/02.Scripts/Monster3.cs
+++ Assets/02.Scripts/Monster3.cs
@@ -7,15 +7,16 @@ public class Monster3 : MonoBehaviour
 {
     RaycastHit hit;
     [SerializeField]
-    private NavMeshAgent monsterAgent;
+    private NavMeshAgent monsterAgent; // 몬스터 네비게이션
 
-    private float MaxDistance = 15f;
+    private float MaxDistance = 15f; // 몬스터의 감지 거리
 
-    private bool isChasing = false;
-    private float chaseDuration = 10f;
+    private bool isChasing = false; // 몬스터의 추격 여부
+    private float chaseDuration = 10f; // 몬스터의 추격 시간
 
     private void Update()
     {
+        // 몬스터가 플레이어를 추격함
         if (Physics.Raycast(transform.position, transform.forward, out hit, MaxDistance))
         {
             if(hit.collider.CompareTag("MONSTER"))
@@ -27,12 +28,14 @@ public class Monster3 : MonoBehaviour
             }
         }
 
+        // isChasing이 true일 때 몬스터는 플레이어를 계속 추격함.
         if(isChasing)
         {
             monsterAgent.SetDestination(transform.position);
         }
     }
 
+    // 몬스터관련 태그를 플레이어가 봤을 경우 몬스터는 10초간 플레이어를 지속적으로 추격함.
     private System.Collections.IEnumerator StopMonster()
     {
         yield return new WaitForSeconds(chaseDuration);
