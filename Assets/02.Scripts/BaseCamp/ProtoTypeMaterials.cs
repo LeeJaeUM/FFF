2c0e18f0620cdc3ecd95a7938b26a6c8b2f41197
LeeJaeUng <lsam3333333@gmail.com>
2024-03-28 15:08:23 +0900

    Revert "Merge branch 'MergeTestMain' into JW_BaseCamp"
    This reverts commit a6a42908d6d86ff8801276443dfdf8cb75f7ccb8, reversing
    changes made to 45b56d4105dc1851c9a888140272395fdddcd684.

---
+++ Assets/02.Scripts/BaseCamp/ProtoTypeMaterials.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtoTypeMaterials : MonoBehaviour
+{
+    public int scrap = 0;           //고철
+    public int mechanicalPart = 0;  //기계부품
+    public int cloth = 0;           //천
+    public int pill = 0;            //알약, 환약
+
+
+}
