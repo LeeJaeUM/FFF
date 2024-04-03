60faaf683ebc3974732b5749f486314aebb7c4ce
LeeJaeUng <lsam3333333@gmail.com>
2024-04-03 14:39:22 +0900

    EnviromentData단순화

--- Assets/02.Scripts/BaseCamp/Block/BlockData/EnviromentData.cs
+++ Assets/02.Scripts/BaseCamp/Block/BlockData/EnviromentData.cs
@@ -5,11 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnviromentData", menuName = "ScriptableObjects/EnviromentData", order = 2)]
 public class EnviromentData : ScriptableObject
 {
-    public enum MaterialType
-    {
-        Wood = 0,
-        Stone
-    }
-    public MaterialType materialType;
     public GameObject enviroPrefab;
 }
