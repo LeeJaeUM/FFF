298ee20c9df4a1e3bd02d926701c96b0312037f7
kkc <94228190+arumolru@users.noreply.github.com>
2024-04-11 14:59:57 +0900

    Stage2 몬스터 추격 개선
    장애물을 밟으면 몬스터가 바로 추격하지 않고 플레이어의 생명이 -1이 되고 생명이 0이 되면 추격하는 방식으로 개선

--- Assets/02.Scripts/Monster/Monster2Ctrl.cs
+++ Assets/02.Scripts/Monster/Monster2Ctrl.cs
@@ -5,5 +5,52 @@ using UnityEngine.AI;
 
 public class Monster2Ctrl : MonoBehaviour
 {
-    
+    [SerializeField]
+    private NavMeshAgent monsterAgent; // Stage2 몬스터의 NavMeshAgent 변수
+    [SerializeField]
+    private AudioSource brokenWoodSound; // 플레이어가 나무를 밟았을 경우 생기는 소리
+    [SerializeField]
+    private Animator monsterAnim;
+
+    private int playerLife = 3; // 플레이어의 생명
+    private bool isWoodExit = false;
+    private bool isChasing = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 플레이어가 BrokenTable을 밟았을 경우
+        if(other.gameObject.CompareTag("WOODTRAP"))
+        {
+            if(!isWoodExit)
+            {
+                //brokenWoodSound.Play();
+                playerLife--; // 플레이어의 생명이 1깎임
+                Debug.Log(playerLife);
+                isWoodExit = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // 플레이어가 BrokenTable에서 중복 충돌을 방지하기 위한 코드
+        if(other.gameObject.CompareTag("WOODTRAP"))
+        {
+            if(isWoodExit)
+            {
+                isWoodExit = false;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        // playerLife가 0이 됬을 경우 몬스터가 플레이어를 추적한다.
+        if(playerLife<=0)
+        {
+            isChasing = true;
+            monsterAgent.SetDestination(gameObject.transform.position);
+            monsterAnim.SetBool("isChasing", isChasing);
+        }
+    }
 }
