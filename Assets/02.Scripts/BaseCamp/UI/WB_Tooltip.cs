31850ad7362cc662fec34c23f069bf3c8f106079
LeeJaeUng <lsam3333333@gmail.com>
2024-04-15 15:20:14 +0900

    스킬 아이콘 클릭 조건 추가

--- Assets/02.Scripts/BaseCamp/UI/WB_Tooltip.cs
+++ Assets/02.Scripts/BaseCamp/UI/WB_Tooltip.cs
@@ -3,15 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using System;
 
 public class WB_Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     GameObject tooltipObj;
 
+    PlayerInputAction inputAction;
+
+    bool canQuit = false;
+
     private void Awake()
     {
         Transform child = transform.GetChild(0);
         tooltipObj = child.gameObject;
+
+        inputAction = new PlayerInputAction();
+    }
+
+    private void OnEnable()
+    {
+        inputAction.UI.Enable();
+        inputAction.UI.Click.performed += OnClickUI;
+    }
+
+    private void OnDisable()
+    {
+        inputAction.UI.Click.performed -= OnClickUI;
+        inputAction.UI.Disable();
+    }
+
+    /// <summary>
+    /// 마우스 왼쪽 버튼 클릭 감지
+    /// </summary>
+    /// <param name="context"></param>
+    private void OnClickUI(InputAction.CallbackContext context)
+    {
+        if(canQuit)
+        {
+            tooltipObj.SetActive(false);
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -21,6 +53,6 @@ public class WB_Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        tooltipObj.SetActive(false);
+        canQuit = true;
     }
 }
