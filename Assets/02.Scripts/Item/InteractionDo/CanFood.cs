058b2d48d6b055d996a09b818482b75e35e595d1
tesia102 <129711130+tesia102@users.noreply.github.com>
2024-04-16 15:20:34 +0900

    4/16 추가
    CanFood, DynamiteBox, GunBox, MagazineBox, OilDrum 등 해야할 일 주석으로 가이드라인 작성. 합친 이후에 코드 수정하면 됨

---
+++ Assets/02.Scripts/Item/InteractionDo/CanFood.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanFood : MonoBehaviour
+{
+    //public void Interact()
+    //{
+    //    // 아이템 사용 시 동작 구현
+    //    // Knife가 인벤토리에 있는지 확인
+    //    if (Inventory.HasItem("Knife"))
+    //    {
+    //        // 특수 상호작용: 체력 회복 동작 구현
+    //        RestoreHealth();
+    //    }
+    //    else
+    //    {
+    //        // 일반 상호작용: 메시지 표시
+    //        Debug.Log("뜯을 만한 것이 필요하다.");
+    //    }
+    //    // 플레이어는 기본적으로 체력이 4로 되어있음.
+    //    // 하루가 지날 때마다 체력이 1씩 줄어들고 0이 되면 게임오버.
+    //    // 맵에는 총 3개의 CanFood가 있고 1개의 CanFood를 먹을 때마다 체력이 3이 증가,
+    //    // 탈출하지 않고 총 10일을 버틸 수 있음.
+    //}
+
+    //private void RestoreHealth()
+    //{
+    //    // 체력 회복 동작 구현
+    //    Inventory.RemoveItem("CanFood");
+    //    Debug.Log("체력이 3 회복되었습니다.");
+    //}
+}
