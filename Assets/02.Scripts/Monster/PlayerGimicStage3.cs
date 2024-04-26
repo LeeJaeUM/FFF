aa8333932996357851c8f14a822818fa709a4aae
kkc <94228190+arumolru@users.noreply.github.com>
2024-04-26 12:14:50 +0900

    3Stage 기믹 제작 중
    완성된 기믹 Diary, PictureFrame

--- Assets/02.Scripts/Monster/PlayerGimicStage3.cs
+++ Assets/02.Scripts/Monster/PlayerGimicStage3.cs
@@ -2,29 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Runtime.CompilerServices;
 
 public class PlayerGimicStage3 : MonoBehaviour
 {
-    RaycastHit hit; // 메인 카메라의 레이 캐스트
+    RaycastHit hit;
 
     [SerializeField]
     private GameObject interactionUI; // 상호작용이 가능한지 나타내는 UI 오브젝트
     [SerializeField]
     private TextMeshProUGUI textField; // 게임의 진행을 위한 Text
     [SerializeField]
-    private GameObject player; // 플레이어 캐릭터
+    private TextMeshProUGUI choiceText1Field; // 선택지 1
     [SerializeField]
-    private GameObject pictureFrameButton; // PictureFrame의 선택지
+    private TextMeshProUGUI choiceText2Field; // 선택지 2
+    [SerializeField]
+    private GameObject player; // 플레이어 캐릭터
+
     [SerializeField]
     private GameObject trap; // Trap 오브젝트
     [SerializeField]
     private GameObject pictureFrame; // PictureFrame 오브젝트
 
-    private int maxDistance = 2; // Raycast 최대 범위
+    [SerializeField]
+    private GameObject[] diaryUI; // Diary UI 오브젝트
+    private int currentPageIndex = 0;
+
+    private float maxDistance = 2f; // Raycast 최대 범위
 
     private bool isInteraction = false; // 플레이어의 상호작용 여부
     private bool textDisplayed = false; // 상호작용 중의 텍스트 중복 방지 여부
     private bool interating = false; // 플레이어가 현재 상호작용 중인지 아닌지 여부
+    private bool choicing = false; // 선택지가 떴을 때 여부
+
+    private bool isEvent = false; // 이벤트 발생 동안
 
     private void Start()
     {
@@ -33,7 +44,7 @@ public class PlayerGimicStage3 : MonoBehaviour
 
     private void Update()
     {
-        if(Physics.Raycast(transform.position,transform.forward,out hit,maxDistance))
+        if(Physics.Raycast(transform.position, transform.forward ,out hit, maxDistance))
         {
             // PictureFrame 상호작용
             if(hit.collider.CompareTag("PICTUREFRAME"))
@@ -50,7 +61,6 @@ public class PlayerGimicStage3 : MonoBehaviour
                     isInteraction = true;
                     interating = true;
                     interactionUI.SetActive(false);
-                    textField.gameObject.SetActive(true); // 텍스트 출력
                     textField.text = "꽤나 큰 액자다...";
                     textDisplayed = true;
                 }
@@ -59,32 +69,117 @@ public class PlayerGimicStage3 : MonoBehaviour
                 else if(Input.GetKeyDown(KeyCode.E) && textDisplayed)
                 {
                     textField.text = "들춰낼까?";
-                    pictureFrameButton.SetActive(true);
+                    choiceText1Field.text = "1. 들춰낸다";
+                    choiceText2Field.text = "2. 내버려둔다";
+                    choicing = true;
                 }
 
                 // 선택지가 출력되었을 때 가능한 버튼
-                if(pictureFrameButton)
+                if(choicing)
                 {
                     // 1번을 누를 때
                     if (Input.GetKeyDown(KeyCode.Alpha1))
                     {
                         textField.text = " "; // 상호작용 종료
-                        pictureFrameButton.SetActive(false); // 상호작용 종료
+                        choiceText1Field.text = " ";
+                        choiceText2Field.text = " ";
                         trap.SetActive(true); // 함정 생성
                         pictureFrame.SetActive(false); // 함정 생성
                         player.SetActive(true); // 상호작용 종료
-                        interating = false; // 상호작용 종료
-                        textDisplayed = false; // 상호작용 종료
+                        isInteraction = false;
+                        textDisplayed = false;
+                        interating = false;
+                        choicing = false;
                     }
 
                     // 2번을 누를 때
                     else if(Input.GetKeyDown(KeyCode.Alpha2))
                     {
                         textField.text = " "; // 상호작용 종료
-                        interating = false; // 상호작용 종료
-                        textDisplayed = false; // 상호작용 종료
+                        choiceText1Field.text = " ";
+                        choiceText2Field.text = " ";
                         player.SetActive(true); // 상호작용 종료
-                        pictureFrameButton.SetActive(false); // 상호작용 종료
+                        isInteraction = false;
+                        textDisplayed = false;
+                        interating = false;
+                        choicing = false;
+                    }
+                }
+            }
+
+            if(hit.collider.CompareTag("DIARY"))
+            {
+                if(!isInteraction)
+                {
+                    interactionUI.SetActive(true);
+                }
+
+                // 상호작용 시작
+                if (Input.GetKeyDown(KeyCode.E) && !textDisplayed && !isEvent)
+                {
+                    isInteraction = true;
+                    interating = true;
+                    interactionUI.SetActive(false);
+                    textField.text = "이 곳 담당의 일기인거 같다.";
+                    textDisplayed = true;
+                }
+
+                // 상호작용 중(E키를 한번 더 누르면 선택지 출력)
+                else if (Input.GetKeyDown(KeyCode.E) && textDisplayed && !isEvent)
+                {
+                    textField.text = "볼까?";
+                    choiceText1Field.text = "1. 본다";
+                    choiceText2Field.text = "2. 내버려둔다";
+                    choicing = true;
+                }
+
+                if(choicing)
+                {
+                    if(Input.GetKeyDown(KeyCode.Alpha1))
+                    {
+                        textField.text = " ";
+                        choiceText1Field.text = " ";
+                        choiceText2Field.text = " ";
+                        choicing = false;
+                        textDisplayed = false;
+                        isEvent = true;
+                        diaryUI[currentPageIndex].SetActive(true);
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.Alpha2))
+                    {
+                        textField.text = " ";
+                        choiceText1Field.text = " ";
+                        choiceText2Field.text = " ";
+                        player.SetActive(true); // 상호작용 종료
+                        isInteraction = false;
+                        textDisplayed = false;
+                        interating = false;
+                        choicing = false;
+                    }
+                }
+
+                if(isEvent && !choicing && !textDisplayed && isInteraction && interating)
+                {
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        diaryUI[currentPageIndex].SetActive(false);
+
+                        currentPageIndex = (currentPageIndex + 1) % diaryUI.Length;
+
+                        diaryUI[currentPageIndex].SetActive(true);
+
+                        if (currentPageIndex == 4)
+                        {
+                            diaryUI[currentPageIndex].SetActive(false);
+
+                            currentPageIndex = 0;
+
+                            isEvent = false;
+                            isInteraction = false;
+                            interating = false;
+                            player.SetActive(true);
+                        }
                     }
                 }
             }
