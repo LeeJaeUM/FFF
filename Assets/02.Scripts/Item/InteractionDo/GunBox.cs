7f8bbb7a5d334b4bd79cb1313df9cc0c5917d90a
tesia102 <129711130+tesia102@users.noreply.github.com>
2024-04-29 15:13:44 +0900

    테스트 씬 추가

--- Assets/02.Scripts/Item/InteractionDo/GunBox.cs
+++ Assets/02.Scripts/Item/InteractionDo/GunBox.cs
@@ -2,22 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunBox : MonoBehaviour
+public class GunBox : MonoBehaviour, IInteractable
 {
-    //public void Interact()
-    //{
-    //    // 인벤토리에 Axe 오브젝트가 있는지 확인
-    //    if (Inventory.HasItem("Axe"))
-    //    {
-    //        // 특수 상호작용: 상자 열기 동작 구현
-    //        OpenBox();
-    //    }
-    //    else
-    //    {
-    //        // 일반 상호작용: 메시지 표시
-    //        Debug.Log("부술 수 있는 도구가 필요합니다.");
-    //    }
-    //}
+    public void Interact()
+    {
+        // 인벤토리에 Axe 오브젝트가 있는지 확인
+        if (Inventory.HasItem("Axe"))
+        {
+            // 특수 상호작용: 상자 열기 동작 구현
+            OpenBox();
+        }
+        else
+        {
+            // 일반 상호작용: 메시지 표시
+            Debug.Log("부술 수 있는 도구가 필요합니다.");
+        }
+    }
 
     //private void OpenBox()
     //{
