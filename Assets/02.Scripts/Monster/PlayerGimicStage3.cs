9268855f76587467c6564ee0866fd4f2a8f4332d
kkc <94228190+arumolru@users.noreply.github.com>
2024-04-30 15:15:03 +0900

    Stage3 기믹 제작중
    Restroom, Lobby일부, Restaurant 제작완료, Storage 제작 중

--- Assets/02.Scripts/Monster/PlayerGimicStage3.cs
+++ Assets/02.Scripts/Monster/PlayerGimicStage3.cs
@@ -42,6 +42,12 @@ public class PlayerGimicStage3 : MonoBehaviour
     private Animator hiddenRoomAnimator; // 비밀문이 열리는 애니메이션
     [SerializeField]
     private Animator clearDoorAnimator; // 클리어 문이 열리는 애니메이션
+    [SerializeField]
+    private Animator startLeftDoor; // 시작 시 처음 열리는 문 애니메이션
+    [SerializeField]
+    private Animator startRightDoor; // 시작 시 처음 열리는 문 애니메이션
+    [SerializeField]
+    private Animator restaurantDoorAnimator; // 식당 문 애니메이션
 
     [SerializeField]
     private GameObject trap; // Trap 오브젝트
@@ -62,6 +68,10 @@ public class PlayerGimicStage3 : MonoBehaviour
     [SerializeField]
     private GameObject remoteControl; // 리모컨 오브젝트
     [SerializeField]
+    private GameObject blade; // Blade 오브젝트
+    [SerializeField]
+    private GameObject Metal; // 쇠로 된 식기 오브젝트
+    [SerializeField]
     private GameObject restaurantRoomDoorKey; // RestaurantRoomDoorKey 오브젝트
     [SerializeField]
     private GameObject restroomCaution; // RestaurantCaution의 자식으로 있는 Text(TMP)
@@ -71,19 +81,25 @@ public class PlayerGimicStage3 : MonoBehaviour
 
     private bool isInteraction = false; // 플레이어의 상호작용 여부
     private bool textDisplayed = false; // 상호작용 중의 텍스트 중복 방지 여부
+    private bool textDisplayed2 = false; // 
     private bool interating = false; // 플레이어가 현재 상호작용 중인지 아닌지 여부
     private bool choicing = false; // 선택지가 떴을 때 여부
     private bool restroomOpen = true; // RestRoomDoor의 열림 여부
     private bool isDiary3Open = false; // 다이어리의 3페이지의 활성 여부
     private bool hiddenRoomDoor = true; // 비밀문의 열림 상태
+    private bool restaurantDoor = true; // 식당문의 열림 상태
 
     private bool isEvent = false; // 이벤트 발생 동안
 
+    private bool startDoorOpen = false; // 시작 문이 열림의 유무
     private bool haveRemoteControl = false; // 인벤토리에 RemoteControl이 있을 경우
     private bool isInteractionTouchPad = false; // TouchPad와 상호작용을 했을 경우
     private bool haveAxe = false; // 인벤토리에 Axe오브젝트가 있을 경우
-    private bool haveRestroomDoorKey = false; // 인벤토리에 RestroomDoorKey가 있을 경우
+    private bool haveRestaurantDoorKey = false; // 인벤토리에 RestaurantDoorKey가 있을 경우
     private bool haveDoctorHand = false; // 인벤토리에 DoctorHand가 있을 경우
+    private bool haveBlade = false; // 인벤토리에 Blade가 있을 경우
+    private bool haveMetal = false; // 인벤토리에 Metal이 있을 경우
+    private bool havePilers = false; // 인벤토리에 Pilers가 있을 경우
     private bool isMoveBookShelf = false; // BookShelf가 이동했을 경우
 
     private bool unLockStorageDoor = false; // StorageDoor의 해금 상태
@@ -104,6 +120,9 @@ public class PlayerGimicStage3 : MonoBehaviour
     {
         Renderer renderer= tvScreen.GetComponent<Renderer>();
 
+        if(!startDoorOpen)
+            StartCoroutine(OpenStartDoor());
+
         if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance))
         {
             Debug.Log(hit.collider.gameObject.name);
@@ -114,6 +133,7 @@ public class PlayerGimicStage3 : MonoBehaviour
                 return;
             }
 
+            // Restroom------------------------------------------------------------------------------------
             // PictureFrame 상호작용
             if (hit.collider.CompareTag("PICTUREFRAME"))
             {
@@ -633,8 +653,8 @@ public class PlayerGimicStage3 : MonoBehaviour
                 }
             }
 
-            // RestroomDoorKey 상호작용
-            if(hit.collider.CompareTag("RESTROOMDOORKEY"))
+            // RestaurantRoomDoorKey 상호작용
+            if (hit.collider.CompareTag("RESTAURANTDOORKEY"))
             {
                 if(!isInteraction)
                 {
@@ -648,7 +668,7 @@ public class PlayerGimicStage3 : MonoBehaviour
                     interating = true;
                     textField.text = "다른 방을 열 수 있는 열쇠인거 같다..";
                     interactionUI.SetActive(false);
-                    haveRestroomDoorKey = true;
+                    haveRestaurantDoorKey = true;
                     textDisplayed = true;
                 }
 
@@ -681,7 +701,6 @@ public class PlayerGimicStage3 : MonoBehaviour
                     interating = true;
                     textField.text = "언제든 떠날 준비였던거 같다.";
                     interactionUI.SetActive(false);
-                    haveRestroomDoorKey = true;
                     textDisplayed = true;
                 }
 
@@ -769,7 +788,6 @@ public class PlayerGimicStage3 : MonoBehaviour
                         interating = true;
                         textField.text = "손을 갖다 대는 걸로 작동하는 패드인거 같다..";
                         interactionUI.SetActive(false);
-                        haveRestroomDoorKey = true;
                         textDisplayed = true;
                         isInteractionTouchPad = true;
                     }
@@ -794,7 +812,6 @@ public class PlayerGimicStage3 : MonoBehaviour
                         interating = true;
                         textField.text = "(절단된 의사의 손을 갖다 댄다..)";
                         interactionUI.SetActive(false);
-                        haveRestroomDoorKey = true;
                         textDisplayed = true;
                         isInteractionTouchPad = true;
                     }
@@ -834,7 +851,6 @@ public class PlayerGimicStage3 : MonoBehaviour
                         interating = true;
                         textField.text = "커다란 책장이다..";
                         interactionUI.SetActive(false);
-                        haveRestroomDoorKey = true;
                         textDisplayed = true;
                         isInteractionTouchPad = true;
                     }
@@ -985,6 +1001,286 @@ public class PlayerGimicStage3 : MonoBehaviour
                     }
                 }
             }
+            // --------------------------------------------------------------------------------
+
+            // Lobby---------------------------------------------------------------------------------
+            // MecanicEye 상호작용
+            if(hit.collider.CompareTag("MECANICEYE"))
+            {
+                if (!isInteraction)
+                {
+                    interactionUI.SetActive(true);
+                }
+
+                // 일반상호작용 시작
+                if (Input.GetKeyDown(KeyCode.E) && !textDisplayed && !textDisplayed2 && !isEvent)
+                {
+                    isInteraction = true;
+                    interating = true;
+                    interactionUI.SetActive(false);
+                    textField.text = "홍채인식으로 작동하는거 같다.";
+                    textDisplayed = true;
+                }
+
+                // 일반상호작용 중
+                else if (Input.GetKeyDown(KeyCode.E) && textDisplayed && !textDisplayed2&& !isEvent)
+                {
+                    textField.text = "지금은 작동하지 않는 거 같다.";
+                    textDisplayed2 = true;
+                }
+
+                // 일반상호작용 종료
+                else if (Input.GetKeyDown(KeyCode.E) && textDisplayed && textDisplayed2 && !isEvent)
+                {
+                    textField.text = " ";
+                    player.SetActive(true);
+                    isInteraction = false;
+                    textDisplayed = false;
+                    textDisplayed2 = false;
+                    interating = false;
+                }
+            }
+
+            // LobbyKeyPad 상호작용
+            if(hit.collider.CompareTag("LOBBYKEYPAD"))
+            {
+                if (!isInteraction)
+                {
+                    interactionUI.SetActive(true);
+                }
+
+                // 일반상호작용 시작
+                if (Input.GetKeyDown(KeyCode.E) && !textDisplayed && !textDisplayed2 && !isEvent)
+                {
+                    isInteraction = true;
+                    interating = true;
+                    interactionUI.SetActive(false);
+                    textField.text = "비밀번호를 입력해야 하는거 같다.";
+                    textDisplayed = true;
+                }
+
+                // 일반상호작용 중
+                else if (Input.GetKeyDown(KeyCode.E) && textDisplayed && !textDisplayed2 && !isEvent)
+                {
+                    textField.text = "지금은 작동하지 않는 거 같다..";
+                    textDisplayed2 = true;
+                }
+
+                // 일반상호작용 종료
+                else if (Input.GetKeyDown(KeyCode.E) && textDisplayed && textDisplayed2 && !isEvent)
+                {
+                    textField.text = " ";
+                    player.SetActive(true);
+                    isInteraction = false;
+                    textDisplayed = false;
+                    textDisplayed2 = false;
+                    interating = false;
+                }
+            }
+            //---------------------------------------------------------------------------------
+
+            // Restaurant ---------------------------------------------------------------------------------
+            // RestaurantDoor 상호작용
+            if(hit.collider.CompareTag("RESTAURANTDOOR"))
+            {
+                if (!isInteraction)
+                {
+                    interactionUI.SetActive(true);
+                }
+
+                // 일반상호작용(식당 문 열쇠가 없을 경우)
+                if(!haveRestaurantDoorKey)
+                {
+                    // 상호작용 시작
+                    if (Input.GetKeyDown(KeyCode.E) && !textDisplayed)
+                    {
+                        isInteraction = true;
+                        interating = true;
+                        interactionUI.SetActive(false);
+                        textField.text = "문이 열리지 않는다.";
+                        textDisplayed = true;
+                    }
+
+                    // 상호작용 종료
+                    else if (Input.GetKeyDown(KeyCode.E) && textDisplayed)
+                    {
+                        textField.text = " ";
+                        player.SetActive(true); // 상호작용 강제 종료
+                        isInteraction = false;
+                        textDisplayed = false;
+                        interating = false;
+                        choicing = false;
+                    }
+                }
+
+                // 특수상호작용(식당 문 열쇠가 있을 경우)
+                else if(haveRestaurantDoorKey)
+                {
+                    // 상호작용 시작, 문이 열리고 닫힘.
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        if (restaurantDoor)
+                        {
+                            restaurantDoorAnimator.SetBool("IsOpen", true);
+                        }
+                        else
+                        {
+                            restaurantDoorAnimator.SetBool("IsOpen", false);
+                        }
+
+                        restaurantDoor = !restaurantDoor;
+                    }
+                }
+            }
+
+            //Metal 상호작용
+            if(hit.collider.CompareTag("METAL"))
+            {
+                if(!isInteraction)
+                {
+                    interactionUI.SetActive(true);
+                }
+
+                // 상호작용 시작
+                if (Input.GetKeyDown(KeyCode.E) && !textDisplayed && !isEvent)
+                {
+                    isInteraction = true;
+                    interating = true;
+                    interactionUI.SetActive(false);
+                    textField.text = "쇠로 된 식기다.";
+                    textDisplayed = true;
+                }
+
+                // 상호작용 중(E키를 한번 더 누르면 선택지 출력)
+                else if (Input.GetKeyDown(KeyCode.E) && textDisplayed && !isEvent)
+                {
+                    textField.text = "쓸 데가 있을까?";
+                    choiceText1Field.text = "1. 줍는다";
+                    choiceText2Field.text = "2. 내버려둔다";
+                    choicing = true;
+                }
+
+                if (choicing)
+                {
+                    // 1번을 누를 때
+                    if (Input.GetKeyDown(KeyCode.Alpha1))
+                    {
+                        textField.text = " ";
+                        choiceText1Field.text = " ";
+                        choiceText2Field.text = " ";
+                        getItemTextField.text = "쇠로 된 식기 획득";
+                        Destroy(Metal); // 식기 비활성화
+                        haveMetal = true;
+
+                        StartCoroutine(GetItem());
+                    }
+
+                    // 2번을 누를 때
+                    else if (Input.GetKeyDown(KeyCode.Alpha2))
+                    {
+                        textField.text = " "; // 상호작용 종료
+                        choiceText1Field.text = " ";
+                        choiceText2Field.text = " ";
+                        player.SetActive(true); // 상호작용 종료
+                        isInteraction = false;
+                        textDisplayed = false;
+                        interating = false;
+                        choicing = false;
+                    }
+                }
+            }
+
+            //-----------------------------------------------------------------------------------------------
+            // Generator 상호작용
+            if(hit.collider.CompareTag("GENERATOR"))
+            {
+                if(!isInteraction)
+                {
+                    interactionUI.SetActive(true);
+                }
+
+                // 일반상호작용(인벤토리에 Pilers가 없을 경우)
+                if(!havePilers)
+                {
+                    // 상호작용 시작
+                    if (Input.GetKeyDown(KeyCode.E) && !textDisplayed && !isEvent)
+                    {
+                        isInteraction = true;
+                        interating = true;
+                        interactionUI.SetActive(false);
+                        textField.text = "문을 열 만한 도구가 필요하다.";
+                        textDisplayed = true;
+                    }
+
+                    // 상호작용 중(E키를 한번 더 누르면 선택지 출력)
+                    else if (Input.GetKeyDown(KeyCode.E) && textDisplayed && !isEvent)
+                    {
+                        textField.text = " ";
+                        player.SetActive(true); // 상호작용 강제 종료
+                        isInteraction = false;
+                        textDisplayed = false;
+                        interating = false;
+                        choicing = false;
+                    }
+                }
+            }
+
+            // Blade 상호작용
+            if(hit.collider.CompareTag("BLADE"))
+            {
+                if(!isInteraction)
+                {
+                    interactionUI.SetActive(true);
+                }
+
+                // 상호작용 시작
+                if (Input.GetKeyDown(KeyCode.E) && !textDisplayed && !isEvent)
+                {
+                    isInteraction = true;
+                    interating = true;
+                    interactionUI.SetActive(false);
+                    textField.text = "꽤나 예리한 칼이다.";
+                    textDisplayed = true;
+                }
+
+                // 상호작용 중(E키를 한번 더 누르면 선택지 출력)
+                else if (Input.GetKeyDown(KeyCode.E) && textDisplayed && !isEvent)
+                {
+                    textField.text = "가지고 있을까?";
+                    choiceText1Field.text = "1. 줍는다";
+                    choiceText2Field.text = "2. 내버려둔다";
+                    choicing = true;
+                }
+
+                if(choicing)
+                {
+                    // 1번을 누를 때
+                    if (Input.GetKeyDown(KeyCode.Alpha1))
+                    {
+                        textField.text = " ";
+                        choiceText1Field.text = " ";
+                        choiceText2Field.text = " ";
+                        getItemTextField.text = "예리한 식칼 획득";
+                        Destroy(blade); // 리모컨 비활성화
+                        haveBlade = true; // RetroTelevision 조건 활성화
+
+                        StartCoroutine(GetItem());
+                    }
+
+                    // 2번을 누를 때
+                    else if (Input.GetKeyDown(KeyCode.Alpha2))
+                    {
+                        textField.text = " "; // 상호작용 종료
+                        choiceText1Field.text = " ";
+                        choiceText2Field.text = " ";
+                        player.SetActive(true); // 상호작용 종료
+                        isInteraction = false;
+                        textDisplayed = false;
+                        interating = false;
+                        choicing = false;
+                    }
+                }
+            }
         }
 
         // 플레이어가 상호작용가능한 오브젝트와 멀어질 경우
@@ -1026,5 +1322,14 @@ public class PlayerGimicStage3 : MonoBehaviour
         textDisplayed = false;
         interating = false;
         choicing = false;
+    }
+
+    IEnumerator OpenStartDoor()
+    {
+        yield return new WaitForSeconds(7);
+
+        startLeftDoor.SetTrigger("IsOpen");
+        startRightDoor.SetTrigger("IsOpen");
+        startDoorOpen = true;
     }
 }
