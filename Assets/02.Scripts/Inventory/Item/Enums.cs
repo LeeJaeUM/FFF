b5b67dc2baec2341e448c4c26425079a11585a5d
switchiz <starter128@gmail.com>
2024-05-03 15:01:54 +0900

    엡데이터

--- Assets/02.Scripts/Inventory/Item/Enums.cs
+++ Assets/02.Scripts/Inventory/Item/Enums.cs
@@ -3,31 +3,37 @@ using UnityEngine;
 
 public enum ItemCode
 {
-    // 재료
+    // Stage1
     Wood = 0,       // 나무
     Ironstone,      // 철광석
     Matches,        // 성냥
     Magazine,       // 잡지
+    CanFood,        // 캔
+    Gun,            // 총
+    Dynamite,       // 다이너마이트
+    FirstDoorKey,   // 첫번째 문 열쇠
+    MasterKey,      // 마스터키
+
+    Knife,          // 칼
+    Pick,           // 곡갱이
+    Axe,            // 도끼
+    WoodenPlanks,   // 나무판자
+    IronPlanks,     // 철판
+
+
+    // Stage2
     Brain,          // 뇌
     Entrails,       // 내장
-
-    // 아이템
-    Dynamite,       // 다이너마이트
-    CanFood,        // 캔
     GasMask,        // 가스마스크
     Syrings,        // 주사기
-    Key,            // 열쇠
     FakeKey,        // 가짜열쇠
-    MasterKey,      // 마스터키
     noteHint,       // 쪽지 힌트
     noteCaution,    // 주의문
     Book,           // 책
-
-    // 장비
-    Gun,            // 총
-    Axe,            // 도끼
     Hammer,         // 망치
     Crowbar,        // 쇠지렛대
+
+    // Stage3
 }
 
 
