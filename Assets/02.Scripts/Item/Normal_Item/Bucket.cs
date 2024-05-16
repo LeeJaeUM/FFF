0546e38fdd10e2dd9b602808792313de0b861d09
tesia102 <129711130+tesia102@users.noreply.github.com>
2024-05-16 14:09:57 +0900

    스테이지1 아이템 기믹완
    G.G

---
+++ Assets/02.Scripts/Item/Normal_Item/Bucket.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucket : MonoBehaviour
+{
+    InventoryUI inventoryUI;
+
+    private void Awake()
+    {
+        inventoryUI = FindAnyObjectByType<InventoryUI>();
+    }
+
+    public void Interact()
+    {
+        inventoryUI.GetItemToSlot(ItemCode.Bucket, 1);
+        Destroy(gameObject);
+    }
+}
