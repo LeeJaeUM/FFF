2e4287af418688416a5566eb749520dfe2b9ba34
LeeJaeUng <lsam3333333@gmail.com>
2024-05-20 14:50:28 +0900

    IronStone 스크립트 변경 및 주위 벽 추가, 시작위치 게이트 애니메이션 추가
    oldPick - Pick 사용
    3회 상호작용으로 파괴 및 애니메이션 추가
    startdoorGate 추가

--- Assets/02.Scripts/Item/InteractionDo/IronStone.cs
+++ Assets/02.Scripts/Item/InteractionDo/IronStone.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 
 public class IronStone : MonoBehaviour, IInteractable
 {
+    readonly ItemCode pick = ItemCode.Pick;
     InventoryUI inventoryUI;
 
-    readonly ItemCode oldPick = ItemCode.OldPick;
+    [SerializeField]
+    private int breakCount = 0; //나무가 도끼질을 버티는 횟수카운트 // 3회
+    readonly int Interact_Hash = Animator.StringToHash("Interact");
+    private Animator animator;
 
     private void Awake()
     {
         inventoryUI = FindAnyObjectByType<InventoryUI>();
+        animator = GetComponent<Animator>();
     }
 
     public void Interact()
     {
-        if(inventoryUI.UseItemCheck(oldPick))
+        if(inventoryUI.UseItemCheck(pick))
         {
-            Destroy(gameObject);
+            BreakStone();
             inventoryUI.GetItemToSlot(ItemCode.Ironstone, 5);
         }
         else
@@ -26,4 +31,17 @@ public class IronStone : MonoBehaviour, IInteractable
             Stage1Manager.Instance.BottomTMPText = "곡괭이가 필요하다.";
         }
     }
+    private void BreakStone()
+    {
+        animator.SetTrigger(Interact_Hash);
+        //AudioManager.instance.PlaySfx(AudioManager.Sfx.ChopTree);
+        inventoryUI.GetItemToSlot(ItemCode.Ironstone, 5);  //stone 얻기 
+        breakCount++;
+        if (breakCount > 2)
+        {
+            //AudioManager.instance.PlaySfx(AudioManager.Sfx.FallingTree);
+            Destroy(gameObject);                    // 돌 없애기
+        }
+    }
+
 }
